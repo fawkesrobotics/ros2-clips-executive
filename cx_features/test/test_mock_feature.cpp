b6182b46d7994b5e68df0835d6754fff6bacf1a0
Ivaylo Doychev <doychev@kbsg.rwth-aachen.de>
2021-06-25 16:02:16 +0200

    cx_features: simple unit test
    To test the interaction between the envManager/envManagerClient
    and the FeaturesManager.

---
+++ cx_features/test/test_mock_feature.cpp
@@ -0,0 +1,118 @@
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "gtest/gtest.h"
+
+#include "ament_index_cpp/get_package_share_directory.hpp"
+
+#include "cx_clips/CLIPSEnvManagerClient.hpp"
+#include "cx_clips/CLIPSEnvManagerNode.h"
+#include "cx_features/ClipsFeaturesManager.hpp"
+#include "cx_features/MockFeature.hpp"
+
+TEST(test_mock_feature, test_context_init) {
+  auto testing_node = rclcpp::Node::make_shared("testing_node");
+  auto manager_node = std::make_shared<cx::CLIPSEnvManagerNode>();
+  auto manager_client = std::make_shared<cx::CLIPSEnvManagerClient>();
+  auto features_manager = std::make_shared<cx::ClipsFeaturesManager>();
+
+  features_manager->pre_configure(manager_node);
+
+  std::vector<std::string> flist;
+
+  const std::string env_name = "executive";
+  const std::string log_name = "(clips-executive)";
+  // std::string pkgPath =
+  // ament_index_cpp::get_package_share_directory("cx_clips");
+
+  rclcpp::executors::MultiThreadedExecutor exe(rclcpp::ExecutorOptions(), 2);
+
+  exe.add_node(manager_node->get_node_base_interface());
+  exe.add_node(features_manager->get_node_base_interface());
+
+  bool finish_exec = false;
+  std::thread t([&]() {
+    while (!finish_exec) {
+      exe.spin();
+    }
+  });
+
+  using lc_tr = lifecycle_msgs::msg::Transition;
+
+  manager_node->trigger_transition(lc_tr::TRANSITION_CONFIGURE);
+  features_manager->trigger_transition(lc_tr::TRANSITION_CONFIGURE);
+
+  {
+    rclcpp::Rate rate(10);
+    auto start_time = testing_node->now();
+    while ((testing_node->now() - start_time).seconds() < 0.5) {
+      rate.sleep();
+    }
+  }
+  // populate the vector with feature names for the req
+  for (auto feat : features_manager->features_) {
+    const std::string &feat_name = feat.second->getFeatureName();
+    flist.push_back(feat_name);
+  }
+
+  manager_node->trigger_transition(lc_tr::TRANSITION_ACTIVATE);
+  features_manager->trigger_transition(lc_tr::TRANSITION_ACTIVATE);
+
+  {
+    rclcpp::Rate rate(10);
+    auto start_time = testing_node->now();
+    while ((testing_node->now() - start_time).seconds() < 0.5) {
+      rate.sleep();
+    }
+  }
+
+  RCLCPP_INFO(testing_node->get_logger(), "TEST-NODE-START");
+
+  // auto created_env = manager_node->getEnvironmentByName(env_name);
+  try {
+
+    ASSERT_TRUE(manager_client->addFeatures(flist));
+    
+    ASSERT_FALSE(manager_client->addFeatures(flist));
+
+    ASSERT_TRUE(manager_client->createNewClipsEnvironment(env_name, log_name));
+
+    auto created_env = manager_node->getEnvironmentByName(env_name);
+
+    RCLCPP_INFO(testing_node->get_logger(), "Before Features Addition!");
+
+    features_manager->clips_env_manager_node_->getEnvironmentByName(env_name)
+        ->evaluate("(ff-feature-request \"mock_feature\")");
+
+    created_env->evaluate("(ff-feature-request \"mock_feature_2\")");
+    created_env->evaluate("(ff-feature-request \"mock_feature_3\")");
+    created_env->evaluate("(ff-feature-request \"mock_feature_4\")");
+    created_env->evaluate("(ff-feature-request \"mock_feature_10\")");
+    created_env->evaluate("(ff-feature-request \"mock_feature_1\")");
+
+    ASSERT_FALSE(manager_client->assertCanRemoveClipsFeatures(flist));
+    // mock_feature->clips_context_init(env_name, created_env);
+
+    ASSERT_TRUE(manager_client->destroyClipsEnvironment(env_name));
+
+    ASSERT_TRUE(manager_client->assertCanRemoveClipsFeatures(flist));
+    ASSERT_TRUE(manager_client->removeClipsFeatures(flist));
+
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  RCLCPP_INFO(testing_node->get_logger(), "TEST-NODE-END");
+
+  finish_exec = true;
+  t.join();
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  rclcpp::init(argc, argv);
+  return RUN_ALL_TESTS();
+}
