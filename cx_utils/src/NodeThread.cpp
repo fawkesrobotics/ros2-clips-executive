5d93c5a6c38d53f6bea13414c441ec3d27c833cc
Ivaylo Doychev <doychev@kbsg.rwth-aachen.de>
2021-08-28 09:19:39 +0300

    cx_utils: add Node thread and service wating utils
    - NodeThread class spins the given node inside a single
    executor and handles all coming callbacks
    - WaitService - as services are called inside already
    spinning node, this class implements wait for future function
    to replace spinning_until_future_complete

---
+++ cx_utils/src/NodeThread.cpp
@@ -0,0 +1,31 @@
+#include <memory>
+
+#include "cx_utils/NodeThread.hpp"
+
+namespace cx {
+
+NodeThread::NodeThread(
+    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_interface)
+    : node_(node_interface) {
+
+  executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+  thread_ = std::make_unique<std::thread>([&]() {
+    executor_->add_node(node_);
+    executor_->spin();
+    executor_->remove_node(node_);
+  });
+
+}
+
+NodeThread::NodeThread(
+    rclcpp::executors::SingleThreadedExecutor::SharedPtr executor)
+    : executor_(executor) {
+  thread_ = std::make_unique<std::thread>([&]() { executor_->spin(); });
+}
+
+NodeThread::~NodeThread() {
+  executor_->cancel();
+  thread_->join();
+}
+
+} // namespace cx
