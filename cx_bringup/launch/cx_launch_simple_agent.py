2130a8cb2146730e34cd78a1e51750d24375ac51
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2024-10-17 19:52:18 +0200

    cx_bringup: make launch file more modular

--- cx_bringup/launch/cx_launch_simple_agent.py
+++ cx_bringup/launch/cx_launch_simple_agent.py
@@ -4,16 +4,34 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.actions import OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
-def generate_launch_description():
+def launch_with_context(context, *args, **kwargs):
     bringup_dir = get_package_share_directory("cx_bringup")
 
     namespace = LaunchConfiguration("namespace")
-    clips_env_manager_file = LaunchConfiguration("clips_env_manager_file")
+    manager_config = LaunchConfiguration("manager_config")
+    manager_config_file = os.path.join(bringup_dir, "params", manager_config.perform(context))
     log_level = LaunchConfiguration("log_level")
+    cx_node = Node(
+        package="cx_bringup",
+        executable="cx_node",
+        output="screen",
+        emulate_tty=True,
+        namespace=namespace,
+        parameters=[
+            manager_config_file,
+        ],
+        arguments=["--ros-args", "--log-level", log_level],
+        # arguments=[('--ros-args --log-level debug')]
+    )
+    return [cx_node]
+
+
+def generate_launch_description():
 
     declare_log_level_ = DeclareLaunchArgument(
         "log_level",
@@ -23,23 +41,10 @@ def generate_launch_description():
 
     declare_namespace_ = DeclareLaunchArgument("namespace", default_value="", description="Default namespace")
 
-    declare_clips_env_manager_file = DeclareLaunchArgument(
-        "clips_env_manager_file",
-        default_value=os.path.join(bringup_dir, "params", "clips_env_manager.yaml"),
-        description="Path to the ROS2 env manager file",
-    )
-
-    cx_node = Node(
-        package="cx_bringup",
-        executable="cx_node",
-        output="screen",
-        emulate_tty=True,
-        namespace=namespace,
-        parameters=[
-            clips_env_manager_file,
-        ],
-        arguments=["--ros-args", "--log-level", log_level],
-        # arguments=[('--ros-args --log-level debug')]
+    declare_manager_config = DeclareLaunchArgument(
+        "manager_config",
+        default_value="clips_env_manager.yaml",
+        description="Name of the CLIPS environment manager configuration",
     )
 
     # The lauchdescription to populate with defined CMDS
@@ -48,6 +53,7 @@ def generate_launch_description():
     ld.add_action(declare_log_level_)
 
     ld.add_action(declare_namespace_)
-    ld.add_action(declare_clips_env_manager_file)
-    ld.add_action(cx_node)
+    ld.add_action(declare_manager_config)
+    ld.add_action(OpaqueFunction(function=launch_with_context))
+
     return ld
