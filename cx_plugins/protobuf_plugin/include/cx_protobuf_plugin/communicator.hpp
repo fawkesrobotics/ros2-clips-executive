e9dea9c71d15cc7988794c75125aa88928a051f7
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2024-11-04 16:42:20 +0100

    protobuf_plugin: fix issues with includes and missing files

--- cx_plugins/protobuf_plugin/include/cx_protobuf_plugin/communicator.hpp
+++ cx_plugins/protobuf_plugin/include/cx_protobuf_plugin/communicator.hpp
@@ -1,4 +1,6 @@
 
+// Licensed under GPLv2. See LICENSE file. Copyright Carologistics.
+
 /***************************************************************************
  *  communicator.h - protobuf network communication for CLIPS
  *
