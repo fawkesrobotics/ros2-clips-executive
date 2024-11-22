f0b00621b6a2536392e85742f86122767bab27a2
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2024-11-22 23:23:01 +0100

    cx_utils: license under apache 2.0

--- cx_utils/include/cx_utils/lock_shared_ptr.hpp
+++ cx_utils/include/cx_utils/lock_shared_ptr.hpp
@@ -1,24 +1,16 @@
-// Licensed under GPLv2. See LICENSE file. Copyright Carologistics.
-
-/***************************************************************************
- *  LockSharedPtr.hpp
- *
- *  Created: 21 June 2021
- *  Copyright  2021  Ivaylo Doychev
- ****************************************************************************/
-
-/*  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  Read the full text in the LICENSE.GPL file in the doc directory.
- */
+// Copyright (c) 2024 Carologistics
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef CX_UTILS_LOCK_SHARED_PTR_HPP
 #define CX_UTILS_LOCK_SHARED_PTR_HPP
